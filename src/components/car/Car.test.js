8933f1030716c5a47d92d27feefaf0ae608fb575
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-14 23:10:00 +0500

    remove car component

--- src/components/car/Car.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Car from './Car';
-
-test('renders kia car', () => {
-  render(<Car name="kia" year="2020"/>);
-  const linkElement = screen.getByText(/kia/i);
-  expect(linkElement).toBeInTheDocument();
-});
