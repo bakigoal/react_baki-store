cdd42b09d373a4ae86c6a6592d517ce556f223f3
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-14 23:14:08 +0500

    add tests

---
+++ src/components/app/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+test('renders kia car', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/React-Bootstrap/i);
+    expect(linkElement).toBeInTheDocument();
+});
