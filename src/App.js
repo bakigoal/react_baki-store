aad473268e12d86fee481f74203e071dd3eb2384
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-15 19:12:03 +0500

    update content

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import {BrowserRouter as Router} from "react-router-dom";
 import Header from "./layouts/Header";
 import Main from "./layouts/Main";
 import Footer from "./layouts/Footer";
-import Container from "react-bootstrap/Container";
 
 export default function App() {
     return (
         <Router>
             <Header/>
-            <Container className="flex-column">
-                <Main/>
-                <Footer/>
-            </Container>
+            <Main/>
+            <Footer/>
         </Router>
     );
 }
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -3,14 +3,17 @@ import Home from "../pages/Home";
 import Users from "../pages/Users";
 import About from "../pages/About";
 import React from "react";
+import Container from "react-bootstrap/Container";
 
 const Main = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="users" element={<Users/>}/>
-            <Route path="about" element={<About/>}/>
-        </Routes>
+        <Container>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="users" element={<Users/>}/>
+                <Route path="about" element={<About/>}/>
+            </Routes>
+        </Container>
     )
 }
 
