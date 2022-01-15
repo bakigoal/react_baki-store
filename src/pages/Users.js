937a4f2bef09ae3ded865b7164bf42ac527c2fb6
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-15 23:40:12 +0500

    render list of elements

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -3,9 +3,42 @@ import React from "react";
 
 const title = "Users";
 
+const list = [
+    {
+        name: 'User 1',
+        age: 23
+    },
+    {
+        name: 'User 2',
+        age: 32
+    },
+    {
+        name: 'User 3',
+        age: 21
+    },
+    {
+        name: 'User 4',
+        age: 45
+    },
+];
+
 const Users = () => {
     useTitle(title)
-    return <h2>Users</h2>;
+
+    let userBlock = user => {
+        return (
+            <div>
+                <h3>{user.name}</h3>
+                <p>Age: {user.age}</p>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            {list.map(user => userBlock(user))}
+        </div>
+    );
 }
 
 export default Users
