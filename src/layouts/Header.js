1079c92bb51145c17c3b9f7c3bac7d1db9f7f717
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-15 19:02:47 +0500

    update header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,20 +1,49 @@
 import Container from "react-bootstrap/Container";
-import {Nav, Navbar} from "react-bootstrap";
+import {Form, FormControl, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import React from "react";
 import {useLocation} from "react-router-dom";
+import logo from '../assets/logo.svg'
+import Button from "react-bootstrap/Button";
 
 const Header = () => {
     return (
-        <Navbar fluid="true" bg="light" expand="lg" sticky="top" className="border-bottom shadow-sm mb-3">
-            <Container>
-                <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav activeKey={useLocation().pathname} className="me-auto">
+        <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="border-bottom shadow-sm mb-3">
+            <Container fluid>
+                <Navbar.Brand href="/">
+                    <img src={logo}
+                         alt=""
+                         width="30"
+                         height="30"
+                         className="d-inline-block align-top"/>{' '}Bakigoal
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll"/>
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        activeKey={useLocation().pathname}
+                        className="me-auto my-2 my-lg-0"
+                        style={{maxHeight: '100px'}}
+                        navbarScroll>
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/about">About</Nav.Link>
                         <Nav.Link href="/users">Users</Nav.Link>
+                        <NavDropdown title="Link" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="/about">About</NavDropdown.Item>
+                            <NavDropdown.Item href="/users">Users</NavDropdown.Item>
+                            <NavDropdown.Divider/>
+                            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#" disabled>
+                            Link
+                        </Nav.Link>
                     </Nav>
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"/>
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
