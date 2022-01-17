2a5b58a963a9d38573f2bbde721f37b3d70d2bd8
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-17 13:17:11 +0500

    separate layout for mobile

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -10,13 +10,24 @@ const Header = () => {
     return (
         <header className="header">
             <Navbar collapseOnSelect bg="white" variant="light" expand="lg" sticky="top" className="border-bottom shadow-sm mb-3">
-                <Container fluid className="ps-5 pe-5">
+                <Container fluid className="ps-5 pe-5 header-desktop">
                     <HeaderBrand/>
                     <Navbar.Toggle aria-controls="navbarScroll" className="header-toggle"/>
-                    <Navbar.Collapse id="navbarScroll" className="header-collapse">
+                    <Navbar.Collapse id="navbarScroll" className="header-collapse flex-grow-0">
                         <HeaderNavigation/>
                     </Navbar.Collapse>
                     <HeaderIcons/>
+                </Container>
+
+                <Container fluid className="header-mobile">
+                    <HeaderBrand/>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <HeaderIcons/>
+                        <Navbar.Toggle aria-controls="navbarScroll" className="header-toggle ms-4"/>
+                    </div>
+                    <Navbar.Collapse id="navbarScroll" className="header-collapse">
+                        <HeaderNavigation/>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
