0124586fb982eba8df91e5728caeb2c3bf970835
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-17 20:30:46 +0500

    add main carousel component

--- src/components/MainCarousel.js
+++ src/components/MainCarousel.js
@@ -2,19 +2,30 @@ import React from "react";
 import {Carousel} from "react-bootstrap";
 import UuidGenerator from "../utils/UuidGenerator";
 import './MainCarousel.scss'
+import Button from "react-bootstrap/Button";
+import {Wave} from "react-animated-text";
 
 const MainCarousel = ({slides}) => (
-    <Carousel variant="dark" className="carousel pt-lg-4 pt-0">
+    <Carousel variant="dark" className="carousel pt-lg-4 pt-0" fade>
         {slides.map(slide => (
             <Carousel.Item key={UuidGenerator.uuid4()} as="div" className="carousel-image-container">
                 <img
                     className="carousel-image"
                     src={slide.img}
-                    alt="First slide"
+                    alt={slide.label}
                 />
                 <Carousel.Caption>
-                    <h3>{slide.label}</h3>
-                    <p>{slide.description}</p>
+                    <h2 className="xl-text1">
+                        <Wave text={slide.label} effect="pop" effectChange={2.2} iterations={1}/>
+                    </h2>
+
+                    <span className="m-text1">
+                        {slide.description}
+                    </span>
+
+                    <div className="pt-2 pb-3">
+                        <Button variant="light">Shop Now</Button>
+                    </div>
                 </Carousel.Caption>
             </Carousel.Item>
         ))}
