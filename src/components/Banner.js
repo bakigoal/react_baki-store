df11f0b940ee06458ba988570c1d343822bd7688
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-17 22:17:16 +0500

    use bootstrap Col component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,26 +3,27 @@ import banner2 from "../assets/images/banner-01.jpg";
 import banner3 from "../assets/images/banner-03.jpg";
 import banner5 from "../assets/images/banner-05.jpg";
 import banner4 from "../assets/images/banner-04.jpg";
-import {Row} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 import ImageBlock from "./ImageBlock";
+import Container from "react-bootstrap/Container";
 
 const Banner = () => (
-    <section className="banner pt-5 pb-5">
+    <Container className="pt-40 pb-5" fluid>
         <Row>
-            <div className="col-sm-10 col-md-8 col-lg-4 ms-auto me-auto">
+            <Col sm={10} md={8} lg={4}>
                 <ImageBlock img={banner1} label="Dresses"/>
                 <ImageBlock img={banner3} label="Sunglasses"/>
-            </div>
-            <div className="col-sm-10 col-md-8 col-lg-4 ms-auto me-auto">
+            </Col>
+            <Col sm={10} md={8} lg={4}>
                 <ImageBlock img={banner5} label="Watches"/>
                 <ImageBlock img={banner2} label="Footwear"/>
-            </div>
-            <div className="col-sm-10 col-md-8 col-lg-4 ms-auto me-auto">
+            </Col>
+            <Col sm={10} md={8} lg={4}>
                 <ImageBlock img={banner4} label="Bags"/>
                 <ImageBlock img={banner3} label="Sunglasses"/>
-            </div>
+            </Col>
         </Row>
-    </section>
+    </Container>
 )
 
 export default Banner
