1b6867ffce2338a22032e0761d20692fd57f9344
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-18 00:57:21 +0500

    fix margin

--- src/components/ImageBlock.js
+++ src/components/ImageBlock.js
@@ -3,7 +3,7 @@ import {Card} from "react-bootstrap";
 import LightButton from "./LightButton";
 
 const ImageBlock = ({img, label}) => (
-    <Card className="text-white mb-3 hov-img-zoom">
+    <Card className="text-white mb-4 hov-img-zoom">
         <Card.Img src={img} alt={label}/>
         <Card.ImgOverlay>
             <div className="h-100 d-flex flex-column-reverse align-items-center">
