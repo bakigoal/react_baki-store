dcad8864d5ebe5072d8992b8625d3961ede17e90
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-17 17:45:26 +0500

    use functional style with react hooks (useState)

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -1,69 +1,50 @@
 import Container from "react-bootstrap/Container";
 import {Navbar} from "react-bootstrap";
-import React from "react";
+import React, {useState} from "react";
 import './Header.scss'
 import HeaderBrand from "./components/HeaderBrand";
 import HeaderNavigation from "./components/HeaderNavigation";
 import HeaderIcons from "./components/HeaderIcons";
 
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cart: {
-                items: this.props.cartItems,
-                visible: false,
-                toggleVisibility: this.toggleHeaderCart
-            }
-        }
-        window.onclick = () => {
-            if (this.state.cart.visible) {
-                this.displayHeaderCart(false)
-            }
-        }
+const Header = (props) => {
+    const [visibleCart, changeCartVisibility] = useState(false)
+
+    window.onclick = () => {
+        changeCartVisibility(false)
     }
 
-    toggleHeaderCart = () => {
-        this.displayHeaderCart(!this.state.cart.visible)
+    const cart = {
+        items: props.cartItems,
+        visible: visibleCart,
+        toggleVisibility: () => changeCartVisibility(!visibleCart)
     }
 
-    displayHeaderCart(visible) {
-        this.setState({
-            cart: {
-                ...this.state.cart,
-                visible: visible,
-            }
-        })
-    }
+    return (
+        <header className="header">
+            <Navbar collapseOnSelect bg="white" variant="light" expand="lg" sticky="top"
+                    className="border-bottom shadow-sm mb-3">
+                <Container fluid className="ps-5 pe-5 header-desktop">
+                    <HeaderBrand/>
+                    <Navbar.Toggle aria-controls="navbarScroll" className="header-toggle"/>
+                    <Navbar.Collapse id="navbarScroll" className="header-collapse flex-grow-0">
+                        <HeaderNavigation/>
+                    </Navbar.Collapse>
+                    <HeaderIcons cart={cart}/>
+                </Container>
 
-    render() {
-        return (
-            <header className="header">
-                <Navbar collapseOnSelect bg="white" variant="light" expand="lg" sticky="top"
-                        className="border-bottom shadow-sm mb-3">
-                    <Container fluid className="ps-5 pe-5 header-desktop">
-                        <HeaderBrand/>
-                        <Navbar.Toggle aria-controls="navbarScroll" className="header-toggle"/>
-                        <Navbar.Collapse id="navbarScroll" className="header-collapse flex-grow-0">
-                            <HeaderNavigation/>
-                        </Navbar.Collapse>
-                        <HeaderIcons cart={this.state.cart}/>
-                    </Container>
-
-                    <Container fluid className="header-mobile">
-                        <HeaderBrand/>
-                        <div className="d-flex justify-content-center align-items-center">
-                            <HeaderIcons cart={this.state.cart}/>
-                            <Navbar.Toggle aria-controls="navbarScroll" className="header-toggle ms-4"/>
-                        </div>
-                        <Navbar.Collapse id="navbarScroll" className="header-collapse border-top">
-                            <HeaderNavigation/>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </header>
-        )
-    }
+                <Container fluid className="header-mobile">
+                    <HeaderBrand/>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <HeaderIcons cart={cart}/>
+                        <Navbar.Toggle aria-controls="navbarScroll" className="header-toggle ms-4"/>
+                    </div>
+                    <Navbar.Collapse id="navbarScroll" className="header-collapse border-top">
+                        <HeaderNavigation/>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
 }
 
 export default Header
--- src/layouts/header/components/HeaderCart.js
+++ src/layouts/header/components/HeaderCart.js
@@ -1,55 +1,48 @@
 import iconHeader2 from "../../../assets/images/icons/icon-header-02.png";
 import './HeaderCart.scss'
 
-import React, {Component} from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
 import HeaderCartItem from "./HeaderCartItem";
 import formatCurrency from "../../../utils/CurrencyFormatter";
 import UuidGenerator from "../../../utils/UuidGenerator";
 
 
-class HeaderCart extends Component {
-
-    toggleCartView(event) {
-        this.props.cart.toggleVisibility()
+const HeaderCart = ({cart}) => {
+    const toggleCartView = event => {
+        cart.toggleVisibility()
         event.stopPropagation()
     }
 
-    onCartClick(event) {
+    const onCartClick = event => {
         event.stopPropagation()
     }
 
-    calcTotalPrice() {
-        return this.props.cart.items.map(item => item.price * item.count).reduce((x, y) => x + y, 0)
-    }
+    const calcTotalPrice = () => cart.items.map(item => item.price * item.count).reduce((x, y) => x + y, 0);
 
-    render() {
-        let cartClass = "header-cart header-dropdown"
-        if (this.props.cart.visible===true) cartClass += " show-header-dropdown"
+    let cartClass = "header-cart header-dropdown"
+    if (cart.visible) cartClass += " show-header-dropdown"
+    return (
+        <div className="header-wrap-icon" onClick={e => toggleCartView(e)}>
+            <img src={iconHeader2} className="header-icon1" alt="ICON"/>
+            <span className="icons-badge">{cart.items.length}</span>
 
-        return (
-            <div className="header-wrap-icon" onClick={e => this.toggleCartView(e)}>
-                <img src={iconHeader2} className="header-icon1" alt="ICON"/>
-                <span className="icons-badge">{this.props.cart.items.length}</span>
+            <div className={cartClass} onClick={e => onCartClick(e)}>
+                <ul className="header-cart-wrapitem ps-0">
+                    {cart.items.map(cartItem => (<HeaderCartItem cartItem={cartItem} key={UuidGenerator.uuid4()}/>))}
+                </ul>
 
-                <div className={cartClass} onClick={e => this.onCartClick(e)}>
-                    <ul className="header-cart-wrapitem ps-0">
-                        {this.props.cart.items.map(cartItem => (
-                            <HeaderCartItem cartItem={cartItem} key={UuidGenerator.uuid4()}/>))}
-                    </ul>
+                <div className="header-cart-total pt-2 pb-4 pr-1">
+                    Total: {formatCurrency(calcTotalPrice())}
+                </div>
 
-                    <div className="header-cart-total pt-2 pb-4 pr-1">
-                        Total: {formatCurrency(this.calcTotalPrice())}
-                    </div>
-
-                    <div className="d-flex gap-3">
-                        <Button variant="dark" type="submit" className="text-uppercase w-100">View Cart</Button>
-                        <Button variant="dark" type="submit" className="text-uppercase w-100">Check Out</Button>
-                    </div>
+                <div className="d-flex gap-3">
+                    <Button variant="dark" type="submit" className="text-uppercase w-100">View Cart</Button>
+                    <Button variant="dark" type="submit" className="text-uppercase w-100">Check Out</Button>
                 </div>
             </div>
-        );
-    }
+        </div>
+    )
 }
 
 export default HeaderCart
