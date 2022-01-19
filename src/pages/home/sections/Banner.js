87ea2068981808a6a3f7e65f5ec7f436b3854e15
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-19 13:07:56 +0500

    fix error

--- src/pages/home/sections/Banner.js
+++ src/pages/home/sections/Banner.js
@@ -1,14 +1,15 @@
 import {Col, Row} from "react-bootstrap";
 import BannerImageBlock from "../../../components/BannerImageBlock";
 import Container from "react-bootstrap/Container";
+import UuidGenerator from "../../../utils/UuidGenerator";
 
 const Banner = ({banners}) => (
     <Container className="banner pt-40 pb-5" as="section">
         <Row className="justify-content-center">
             {banners.map(col => (
-                <Col sm={10} md={8} lg={4}>
+                <Col sm={10} md={8} lg={4} key={UuidGenerator.uuid4()}>
                     {col.map(item => (
-                        <BannerImageBlock img={item.img} label={item.label}/>
+                        <BannerImageBlock img={item.img} label={item.label} key={UuidGenerator.uuid4()}/>
                     ))}
                 </Col>
             ))}
