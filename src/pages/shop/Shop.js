a5fb9244695e990315f49692b34013170b353f3b
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-19 21:42:16 +0500

    add products

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -6,22 +6,27 @@ import ProductList from "./sections/ProductList";
 import banner from '../../assets/images/heading-pages-01.jpg'
 import PageTitleBanner from "../../components/PageTitleBanner";
 import ProductSortAndPagination from "./sections/ProductSortAndPagination";
+import {productService} from "../../service/ProductService";
 
-const Shop = () => (
-    <>
-        <PageTitleBanner title="Products" img={banner}/>
-        <Container className="pt-40">
-            <Row>
-                <Col sm={6} md={4} lg={3} className="pb-40">
-                    <ProductFilterSidebar/>
-                </Col>
-                <Col sm={6} md={8} lg={9} className="pb-40">
-                    <ProductSortAndPagination/>
-                    <ProductList/>
-                </Col>
-            </Row>
-        </Container>
-    </>
-)
+const Shop = ({setCartItems}) => {
+    const products = productService.getAllProducts()
+
+    return (
+        <>
+            <PageTitleBanner title="Products" img={banner}/>
+            <Container className="pt-40">
+                <Row>
+                    <Col sm={6} md={4} lg={3} className="pb-40">
+                        <ProductFilterSidebar/>
+                    </Col>
+                    <Col sm={6} md={8} lg={9} className="pb-40">
+                        <ProductSortAndPagination/>
+                        <ProductList products={products} setCartItems={setCartItems}/>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
 
 export default Shop
