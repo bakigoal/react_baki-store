498fd753547108b1402453e7bcab37eab1c56be2
Ildar <bakirovildar2011@mail.ru>
2022-01-20 11:30:31 +0500

    refactoring Sidebar3

--- src/pages/blog/sections/sidebar/Categories.js
+++ src/pages/blog/sections/sidebar/Categories.js
@@ -3,7 +3,7 @@ import '../../Blog.scss'
 const data = ['Fashion','Beauty','Street Style','Life Style','DIY & Crafts']
 
 const Categories = () => (
-    <div className="pb-5 pt-5 sidebarCategories m-text23">
+    <div className="pb-5 pt-5 sidebar-categories m-text23">
         <h4>Categories</h4>
         <ul>
             {data.map((list,index) => (
