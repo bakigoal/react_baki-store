e452c2dc1182c2d946454c2bd6816374311e2cc7
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-21 10:34:45 +0500

    fix failed ci job

--- src/layouts/main/Main.js
+++ src/layouts/main/Main.js
@@ -1,4 +1,4 @@
-import {Navigate, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "../../pages/home/Home";
 import Contact from "../../pages/Contact";
 import About from "../../pages/About";
